a37f85c170a271db3c204cce44397119e6e3be22
JDreamms <1409135@rgu.ac.uk>
2016-04-10 13:02:10 +0100

    updated added login screen

--- app/src/main/java/com/example/david/reddit_android/LoginActivity.java
+++ app/src/main/java/com/example/david/reddit_android/LoginActivity.java
@@ -6,8 +6,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity  {
+
+    Button loginBtn;
+    EditText usernameTxt;
+    EditText passwordTxt;
+    String username;
+    String password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +24,16 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        loginBtn = (Button) findViewById(R.id.login_button);
+        usernameTxt = (EditText) findViewById(R.id.username);
+        passwordTxt = (EditText) findViewById(R.id.password);
 
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 
