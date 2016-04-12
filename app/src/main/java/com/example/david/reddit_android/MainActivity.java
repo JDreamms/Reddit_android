2cb86fa5b0749107f2ae9d80141cea7a643ab05c
JDreamms <1409135@rgu.ac.uk>
2016-04-12 20:47:27 +0100

    added subbreddits page todo: add oauth process to fill in subreddits after user has logged in until then, button to take you to subreddits takes you to login page/.

--- app/src/main/java/com/example/david/reddit_android/MainActivity.java
+++ app/src/main/java/com/example/david/reddit_android/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.david.reddit_android;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,10 +17,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity
 
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +64,13 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
-
-    void addFragment(){
+    void addFragment() {
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.fragments_holder
@@ -60,6 +78,29 @@ public class MainActivity extends AppCompatActivity
                 .commit();
     }
 
+    static public String convertToCacheName(String url) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            digest.update(url.getBytes());
+            byte[] b = digest.digest();
+            BigInteger bi = new BigInteger(b);
+            return "mycache_" + bi.toString(16) + ".cac";
+        } catch (Exception e) {
+            Log.e("MD5", e.toString());
+            return null;
+        }
+    }
+
+
+    private static boolean tooOld(long time){
+        long now=new Date().getTime();
+        long diff=now-time;
+        if(diff>1000*60*5) // 5 minutes
+            return true;
+        return false;
+    }
+
+    
 
 
     @Override
@@ -120,5 +161,45 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.david.reddit_android/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.david.reddit_android/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
---
+++ app/src/main/java/com/example/david/reddit_android/PostToReddit.java
@@ -0,0 +1,13 @@
+package com.example.david.reddit_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostToReddit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_to_reddit);
+    }
+}
